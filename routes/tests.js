eb4afce309b6131e38bf368b37fef5e525c7a4ab
aquatec9256 <aquatec9256@gmail.com>
2019-11-01 12:11:29 +0530

    mail test transporter in tests

--- routes/tests.js
+++ routes/tests.js
@@ -2,7 +2,13 @@ const express = require('express');
 const router = express.Router();
 const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
 const nodemailer = require('nodemailer');
-
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'aquatecinnovative1@gmail.com',
+        pass: 'aquatec@321'
+    }
+  });
 var mailOptions = {
     from: 'aquatecinnovative1@gmail.com',
     to: 'sekhon.game@gmail.com',
