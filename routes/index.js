b50e721e6dbcbcaec902ee428fb8848208a768db
aquatec9256 <aquatec9256@gmail.com>
2019-11-01 13:49:09 +0530

    test madd

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,34 @@
 const express = require('express');
 const router = express.Router();
 const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
+const nodemailer = require('nodemailer');
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'aquatecinnovative1@gmail.com',
+        pass: 'aquatec@321'
+    }
+  });
+var mailOptions = {
+    from: 'aquatecinnovative1@gmail.com',
+    to: 'sekhon.game@gmail.com',
+    subject: 'Sending Email using Node.js',
+    text: 'That was easy!'
+  };
+  
 
 // Welcome Page
 router.get('/', forwardAuthenticated, (req, res) => res.render('welcome'));
-router.get('/mailtest', forwardAuthenticated, (req, res) => res.render('mailtest'));
+router.get('/mailtest', forwardAuthenticated, (req, res) =>{
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+  res.render('mailtest');
+} );
 // Dashboard
 router.get('/dashboard', ensureAuthenticated, (req, res) =>
   res.render('dashboard', {
