e093122d617e9925ee707c5418010377de440130
myceo <myceo360@gmail.com>
2019-11-13 15:04:24 +0530

    order not found issue d

--- routes/products.js
+++ routes/products.js
@@ -22,4 +22,24 @@ router.get('/', function(req, res, next) {
             })
         })
   });
+  router.get('/:id', function(req, res, next) {
+    var perPage = 9;
+    var page = req.params.page || 1;
+  
+    Product
+        .find({})
+        .skip((perPage * page) - perPage)
+        .limit(perPage)
+        .exec(function(err, products) {
+            Product.count().exec(function(err, count) {
+                if (err) return next(err)
+                res.render('pages/public/product', {
+                    products: products,
+                    current: page,
+                    pages: Math.ceil(count / perPage),
+                    layout:'layout'
+                })
+            })
+        })
+  });
   module.exports = router;
