8d84eb416c2dd3586c515b2abcd072877e2836ed
myceo <myceo360@gmail.com>
2019-11-29 19:19:49 +0530

    paypal

--- routes/payments.js
+++ routes/payments.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var router = express.Router();
 var braintree = require('braintree');
-
+const  gateway = braintree.connect({
+  environment: braintree.Environment.Sandbox,
+  // Use your own credentials from the sandbox Control Panel here
+  merchantId: 'dwt5m34ppngz6s7k',
+  publicKey: 'g2d976m7dxpt6bx5',
+  privateKey: '117df9268ade2b95fc3f526966441059'
+});
 router.post('/paypal', function(req, res, next) {
-  var gateway = braintree.connect({
-    environment: braintree.Environment.Sandbox,
-    // Use your own credentials from the sandbox Control Panel here
-    merchantId: 'dwt5m34ppngz6s7k',
-    publicKey: 'g2d976m7dxpt6bx5',
-    privateKey: '117df9268ade2b95fc3f526966441059'
-  });
+ 
 
   // Use the payment method nonce here
   var nonceFromTheClient = req.body.paymentMethodNonce;
@@ -36,7 +36,7 @@ router.post('/hosted', function(req, res, next) {
     var nonceFromTheClient = req.body.paymentMethodNonce;
     // Create a new transaction for $10
     var newTransaction = gateway.transaction.sale({
-      amount: '23.23',
+      amount: '26.23',
       paymentMethodNonce: nonceFromTheClient,
       options: {
         // This option requests the funds from the transaction
