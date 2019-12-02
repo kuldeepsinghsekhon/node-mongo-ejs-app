cea3cf9708084ea0818eabfc392f25472f308842
myceo <myceo360@gmail.com>
2019-12-02 19:02:54 +0530

    database design

--- models/Product.js
+++ models/Product.js
@@ -7,12 +7,27 @@ const mongoose = require('mongoose');
 // - Creating Schema for database - \\
 const prodSchema = new mongoose.Schema({
  name: String,
+ department:String,
+ brand:String,
  description: String,
  category: String,
  sku: Number,
  price: Number,
  style:String,
- image: String
+ image: String,
+ //  Details: { 
+//     type: mongoose.Schema.Types.ObjectId, 
+//     ref: 'Detail' 
+//   },
+ Condition:String,
+//  attrs: { 
+//     type: mongoose.Schema.Types.ObjectId, 
+//     ref: 'Attr' //color,size etc 
+//   },
+//  images: { 
+//     type: mongoose.Schema.Types.ObjectId, 
+//     ref: 'Image' //multiple image 
+//   }
 });
 // - Compiling mongoose Schema to a Model - \\
 const Product = mongoose.model('Product', prodSchema);
