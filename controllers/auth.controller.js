617e159c6b60a6fcc3848611d05fafdaf7e63031
kuldeep singh <mca.kuldeep.sekhon@gmail.com>
2020-01-03 20:10:13 +0530

    profile pe

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -198,7 +198,6 @@ exports.signIn=function(req, res, next){
     exports.profile=function(req, res, next) {
       User
       .findOne({user:req.user})
-          .limit(perPage)
           .exec(function(err, users) {
               User.count().exec(function(err, count) {
                   if (err) return next(err)
