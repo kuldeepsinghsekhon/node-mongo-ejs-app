e5496d326252434f1267538a8befe4b3610126ae
kuldeep singh <mca.kuldeep.sekhon@gmail.com>
2020-01-03 11:05:37 +0530

    last name in user model

--- models/User.js
+++ models/User.js
@@ -7,6 +7,10 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: true
   },
+  lastname: {
+    type: String,
+    required: true
+  },
   username: {
     type: String,
     required: true
