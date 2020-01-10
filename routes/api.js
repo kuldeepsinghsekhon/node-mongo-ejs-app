c2339db580171f4ccf70d69428e50f4832773954
aquatecinnovative <aqutecinnovative1@gmail.com>
2020-01-10 13:38:55 +0530

    API saveSeller Info

--- routes/api.js
+++ routes/api.js
@@ -6,7 +6,7 @@ const router = express.Router();
 const settings_controller = require('../controllers/settings.controller');
 const product_controller = require('../controllers/api.product.controller');
 const auth_controller = require('../controllers/auth.controller');
-
+const utils_controller = require('../controllers/utils.controller');
 router.post('/products/',passport.authenticate(['basic'], { session : false }),product_controller.products);
 router.get('/products/:id',forwardAuthenticated, product_controller.findById );
 router.get('/products/sell/:id',ensureAuthenticated, product_controller.sellProductVariant );
@@ -21,4 +21,8 @@ router.post('/user/settings/resetpsword',passport.authenticate('basic', { sessio
 router.post('/change_password',passport.authenticate('basic', { session : false }), auth_controller.updateChangePassword);
 router.post('/settings/profile',passport.authenticate('basic', { session : false }), settings_controller.saveProfile);
 router.get('/countries',forwardAuthenticated, utils_controller.allCoutries);
+router.post('/settings/seller-info',passport.authenticate('basic', { session : false }), settings_controller.saveSellerInfo);
+router.post('/settings/shipping-info',passport.authenticate('basic', { session : false }), settings_controller.saveShippingInfo);
+router.post('/settings/buyer-info',passport.authenticate('basic', { session : false }), settings_controller.saveBuyerInfo);
+
 module.exports = router;
